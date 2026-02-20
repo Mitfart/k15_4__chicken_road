9c0d5adb153b9c090cdd6135f400ecf67941dd69
Mitfart <mr.mitfart@mail.ru>
2026-02-20 15:36:08 +0400

    Fix: Chest

--- src/UI/Chest.ts
+++ src/UI/Chest.ts
@@ -17,7 +17,6 @@ import {gsap} from "gsap";
 import {PixiPlugin} from "gsap/PixiPlugin";
 import {Game} from "../../plugins/Game/Game.ts";
 import {WidgetRoot} from "../../plugins/Game/UI.ts";
-import {LoadAssetsDB} from "../../plugins/Assets/LoadAssetsDB.ts";
 import {ScreenContainer} from "../../plugins/Utils/Components/ScreenContainer.ts";
 import {Cover} from "../../plugins/Utils/Components/Cover.ts";
 import {OnClick} from "../../plugins/Utils/UIEvents.ts";
@@ -38,14 +37,10 @@ export type ChestScreen = {
 }
 
 export default class Chest {
-    private static _chest: ChestScreen | null = null;
+    private static _chest: ChestScreen;
 
     public static async Show(game: Game, onComplete?: () => void): Promise<void> {
-        if (this._chest) {
-            return;
-        }
-
-        this._chest = await this.Construct(game, onComplete);
+        this._chest ??= await this.Construct(game, onComplete);
 
         await Promise.all([
             this._chest.cover.show(),
@@ -54,23 +49,21 @@ export default class Chest {
 
         this.waitForOpenClick();
     }
+
     public static async Hide(): Promise<void> {
         if (!this._chest) return;
 
-        const onComplete = this._chest.onComplete;
+        if (this._chest.onComplete) this._chest.onComplete();
 
-        await this._chest.screen.hide();
-
-        await this._chest.cover.hide();
+        await Promise.all([
+            this._chest.cover.hide(),
+            this._chest.screen.hide()
+        ]);
 
         this._chest.game.ui.remove(this._chest.screen);
         this._chest.game.ui.remove(this._chest.cover);
-        this._chest = null;
-
-        if (onComplete) {
-            onComplete();
-        }
     }
+
     private static async Construct(game: Game, onComplete?: () => void): Promise<ChestScreen> {
         // Создаем затемнение фона (добавляем первым, чтобы был за сундуком)
         const cover = game.ui.add(new Cover(game.resizer, 0.6, 0.3), WidgetRoot.CENTER);
@@ -84,18 +77,14 @@ export default class Chest {
         const chestSprite = screen.addChild(AnimatedSprite.fromFrames(closedFrames));
         chestSprite.anchor.set(0.5);
         chestSprite.animationSpeed = 1 / closedFrames.length * 10; 
-        chestSprite.loop = true;
-        chestSprite.play();
+        chestSprite.loop = false;
 
-        let openedSprite: AnimatedSprite | undefined;
         const openedFrames = animations["opened"];
-        if (openedFrames && Array.isArray(openedFrames) && openedFrames.length > 0) {
-            openedSprite = screen.addChild(AnimatedSprite.fromFrames(openedFrames));
-            openedSprite.anchor.set(0.5);
-            openedSprite.animationSpeed = 1 / openedFrames.length * 10; 
-            openedSprite.loop = true; 
-            openedSprite.visible = false; 
-        }
+        const openedSprite = screen.addChild(AnimatedSprite.fromFrames(openedFrames));
+        openedSprite.anchor.set(0.5);
+        openedSprite.animationSpeed = 1 / openedFrames.length * 10;
+        openedSprite.loop = false;
+        openedSprite.visible = false;
 
         return {
             screen,
@@ -125,71 +114,18 @@ export default class Chest {
         if (!this._chest || this._chest.isOpened) return;
 
         this._chest.isOpened = true;
-
-        let chestData: any;
-        try {
-            const assetsDB = await LoadAssetsDB();
-            
-            if (!assetsDB || !assetsDB.data || !assetsDB.data.chest) {
-                return;
-            }
-            
-            chestData = assetsDB.data.chest;
-            
-            if (!chestData || typeof chestData !== 'object') {
-                return;
-            }
-        } catch (error: any) {
-            return;
-        }
-        
-        let animations = chestData.animations;
-        
-        if (!animations && chestData.frames && typeof chestData.frames === 'object') {
-            const frameKeys = Object.keys(chestData.frames);
-            
-            if (frameKeys.length >= 10) {
-                const closedFrames = frameKeys.slice(0, 10);
-                const openFrames = frameKeys.slice(10);
-                const openedFrames = frameKeys.slice(-1);
-                
-                animations = {
-                    closed: closedFrames,
-                    open: openFrames,
-                    opened: openedFrames
-                };
-            }
-        }
-        
-        if (!animations) {
-            return;
-        }
-        const openFrames = animations["open"];
-
-        if (!openFrames || !Array.isArray(openFrames) || openFrames.length === 0) {
-            return;
-        }
-
         this._chest.chestSprite.stop();
         this._chest.chestSprite.visible = false;
+
+        const openFrames = Assets.get(AssetsDB.data.chest).animations["open"];
         const openSprite = this._chest.screen.addChild(AnimatedSprite.fromFrames(openFrames));
         openSprite.anchor.set(0.5);
-        openSprite.loop = false; 
-        openSprite.animationSpeed = 1 / openFrames.length * 15; 
+        openSprite.loop = false;
+        openSprite.animationSpeed = 1 / openFrames.length * 10;
         openSprite.play();
 
         openSprite.onComplete = () => {
             openSprite.destroy();
-
-            if (!this._chest) return;
-            
-            if (this._chest.openedSprite) {
-                this._chest.openedSprite.visible = true;
-                this._chest.openedSprite.play();
-            } else {
-                this._chest.chestSprite.visible = true;
-                this._chest.chestSprite.play();
-            }
 
             Panel.Show(this._chest.game, {
                 text: "CONGRATULATIONS",
